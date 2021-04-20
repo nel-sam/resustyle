90eb87ef0fae393a19b18269cbf613cf46116887
Nelson <nelson.rs.07@gmail.com>
2021-04-19 22:31:36 -0700

    Added AdSpace

---
+++ src/components/adspace/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './adspace.scss';
+
+const AdSpace: React.FC = () => {
+  const lines = (n: number) => {
+    const stars = [];
+    for (let i = 0; i < n; ++i) {
+      stars.push(<p key={i}>Advertising Space</p>);
+    }
+
+    return stars;
+  };
+
+  return (
+    <div className="adspace">
+      {lines(20)}
+    </div>);
+};
+
+export default AdSpace;
